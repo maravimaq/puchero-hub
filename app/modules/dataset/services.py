23584653cd3a7162b699f5329e8927cd37bc835d
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-10 21:42:08 +0100

    Feat(dataset): Save converted .json into .zip to download them

--- app/modules/dataset/services.py
+++ app/modules/dataset/services.py
@@ -196,7 +196,7 @@ class DataSetService(BaseService):
 
                 if os.path.isdir(user_folder_path):
                     dataset_folders = [folder for folder in os.listdir(user_folder_path)
-                                       if folder.startswith("dataset_")]
+                                   if folder.startswith("dataset_")]
 
                     for dataset_folder in dataset_folders:
                         dataset_folder_path = os.path.join(user_folder_path, dataset_folder)
@@ -213,12 +213,21 @@ class DataSetService(BaseService):
 
                                         pdf_file_path = file_path.replace('.uvl', '.pdf')
                                         convert_uvl_to_pdf(file_path, pdf_file_path)
-
+                                    
                                         if os.path.exists(pdf_file_path):
                                             zip_file.write(pdf_file_path, arcname=os.path.join(dataset_folder, 
                                                                                                relative_path.replace(
                                                                                                    '.uvl', '.pdf')))
                                             os.remove(pdf_file_path)
+
+                                        json_file_path = file_path.replace('.uvl', '.json')
+                                        convert_uvl_to_json(file_path, json_file_path)
+                                    
+                                        if os.path.exists(json_file_path):
+                                            zip_file.write(json_file_path, arcname=os.path.join(dataset_folder,
+                                                                                                relative_path.replace(
+                                                                                                    '.uvl', '.json')))
+                                            os.remove(json_file_path)
 
         return full_archive_path
 
