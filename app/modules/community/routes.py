575cfa16ea3ca626bcaa19071bc39cb5a9b2e0f4
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-11 23:02:52 +0100

    Feat(community): Created route methods to create communities

--- app/modules/community/routes.py
+++ app/modules/community/routes.py
@@ -20,3 +20,24 @@ def joined_communities():
     form = CommunityForm()
     communities = community_service.get_all_joined_by_user(current_user.id)
     return render_template('community/index.html', communities=communities, form=form)
+
+
+@community_bp.route('/community/create', methods=['GET', 'POST'])
+@login_required
+def create_community():
+    form = CommunityForm()
+    if form.validate_on_submit():
+        result = community_service.create(
+            name=form.name.data,
+            description=form.description.data,
+            owner_id=current_user.id  # Se asigna el usuario autenticado como propietario
+        )
+        return community_service.handle_service_response(
+            result=result,
+            errors=form.errors,
+            success_url_redirect='community.index',
+            success_msg='Community created successfully!',
+            error_template='community/create.html',
+            form=form
+        )
+    return render_template('community/create.html', form=form)
