02097ab6818c8d74cb9bfb3ed7356219af3ad4a0
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-11 23:00:31 +0100

    Feat(community): Created service methods to get all communities created by the user authenticated and all the repositories they are members of

---
+++ app/modules/community/services.py
@@ -0,0 +1,13 @@
+from app.modules.community.repositories import CommunityRepository
+from core.services.BaseService import BaseService
+
+
+class CommunityService(BaseService):
+    def __init__(self):
+        super().__init__(CommunityRepository())
+
+    def get_all_by_user(self, user_id):
+        return self.repository.get_all_by_user(user_id)
+
+    def get_all_joined_by_user(self, user_id):
+        return self.repository.get_all_joined_by_user(user_id)
