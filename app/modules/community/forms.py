6fb3c01163e18e0fe3e53bb3c3e097508c3b2a67
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-12 18:36:11 +0100

    Fix(community):Deleted unnecesary camps in Community Form

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -6,6 +6,4 @@ from wtforms.validators import DataRequired, Length, Optional
 class CommunityForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=256)])
     description = TextAreaField('Description', validators=[Optional(), Length(max=1024)])
-    created_at = DateTimeField('Created At', format='%Y-%m-%d %H:%M:%S', validators=[Optional()])
-    owner_id = IntegerField('Owner ID', validators=[DataRequired()])  # Normalmente no se expone en formularios de usuario
     submit = SubmitField('Create Community')
