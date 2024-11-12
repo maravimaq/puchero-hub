27c9f1dacd1b4b60256af422ae6692d280c910ae
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-12 18:37:06 +0100

    Fix(community):Reformed members association

--- app/modules/community/models.py
+++ app/modules/community/models.py
@@ -1,6 +1,10 @@
 from app import db
 from datetime import datetime
 
+members_association = db.Table('members_association',
+    db.Column('community_id', db.Integer, db.ForeignKey('community.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    )
 
 class Community(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,13 +14,7 @@ class Community(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     owner = db.relationship('User', backref='owned_communities', lazy=True)
-    members = db.relationship('User', secondary='community_members', backref='communities', lazy='dynamic')
-
-    community_members = db.Table(
-        'community_members',
-        db.Column('community_id', db.Integer, db.ForeignKey('community.id'), primary_key=True),
-        db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    )
+    members = db.relationship('User', secondary=members_association, backref='joined_communities', lazy='dynamic')
 
     def __repr__(self):
         return f'Community<{self.id}, Name={self.name}, Owner={self.owner.username}>'
