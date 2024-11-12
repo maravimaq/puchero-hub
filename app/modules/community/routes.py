8d53409eb00ba8623603ef1b876dcb307ed5962e
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-12 18:46:15 +0100

    Fix(community):Remade create method

--- app/modules/community/routes.py
+++ app/modules/community/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 
+from app import db
+from app.modules.community.models import Community
 from app.modules.community.forms import CommunityForm
 from app.modules.community import community_bp
 from app.modules.community.services import CommunityService
@@ -27,17 +29,23 @@ def joined_communities():
 def create_community():
     form = CommunityForm()
     if form.validate_on_submit():
-        result = community_service.create(
+        community = Community(
             name=form.name.data,
             description=form.description.data,
-            owner_id=current_user.id  # Se asigna el usuario autenticado como propietario
+            owner_id=current_user.id
         )
-        return community_service.handle_service_response(
-            result=result,
-            errors=form.errors,
-            success_url_redirect='community.index',
-            success_msg='Community created successfully!',
-            error_template='community/create.html',
-            form=form
-        )
+        
+        community.members.append(current_user)
+        
+        try:
+            db.session.add(community)
+            db.session.commit()
+            flash('Community created successfully!', 'success')
+            return redirect(url_for('community.my_communities'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error creating community: {str(e)}', 'danger')
+            return render_template('community/create.html', form=form)
+
     return render_template('community/create.html', form=form)
+
