b2139c2dfc2823cd841ddb15f340154ffc062580
María de Mar Ávila Maqueda <maravimaq@alum.us.es>
2024-11-12 20:06:30 +0100

    Fix(communities): Modified relation between members and communities

--- app/modules/community/models.py
+++ app/modules/community/models.py
@@ -2,9 +2,9 @@ from app import db
 from datetime import datetime
 
 members_association = db.Table('members_association',
-    db.Column('community_id', db.Integer, db.ForeignKey('community.id'), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    )
+                               db.Column('community_id', db.Integer, db.ForeignKey('community.id'), primary_key=True),
+                               db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True))
+
 
 class Community(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,7 +14,7 @@ class Community(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     owner = db.relationship('User', backref='owned_communities', lazy=True)
-    members = db.relationship('User', secondary=members_association, backref='joined_communities', lazy='dynamic')
+    members = db.relationship('User', secondary=members_association, backref='joined_communities', lazy='subquery')
 
     def __repr__(self):
         return f'Community<{self.id}, Name={self.name}, Owner={self.owner.username}>'
